e3318a84a6b169857c57821871e06f6e7be45d88
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2021-11-09 22:07:08 +0100

    test change

--- controllers/user/uploadphoto.js
+++ controllers/user/uploadphoto.js
@@ -8,10 +8,6 @@ const ProfilePhotoUpload = async (req, res) => {
   try {
     const { userid } = req.params;
 
-    let loggedInUser = await User.findOne({ userid });
-    if (!loggedInUser)
-      return res.status(400).json({ msg: "Please login to continue" });
-
     await cloudinarySetUp();
 
     const uploadedImage = await cloudinary.uploader.upload(req.file.path, {
@@ -20,19 +16,9 @@ const ProfilePhotoUpload = async (req, res) => {
         { height: 250, width: 250, crop: "fill" },
       ],
     });
-
-    if (!uploadedImage)
-      return res.status(500).json({ msg: "An error occurred while uploading" });
-
-    loggedInUser.avatarSmall = uploadedImage.eager[0].secure_url;
-    loggedInUser.avatar = uploadedImage.eager[1].secure_url;
-
-    await loggedInUser.save();
-
-    return res.status(201).json({
-      msg: "Profile image updated successfully",
-      userdata: loggedInUser,
-      img: uploadedImage.eager[0].secure_url,
+    let founduser = await User.findOne({ userid });
+    await founduser.updateOne({
+      $set: { avatarSmall: uploadedImage.eager[0].secure_url },
     });
   } catch (err) {
     console.log("error", err.message);
